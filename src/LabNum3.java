076084df279c37a18a0e822eac60fe6692c65add
Michelle <michellehk1993@gmail.com>
2018-07-17 17:15:57 -0400

    added comments

--- src/LabNum3.java
+++ src/LabNum3.java
@@ -5,9 +5,9 @@ public class LabNum3 {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 
-		String answer;
+		String answer; //string is created for later uses and has to be placed outside of the do while loop
 		
-		System.out.println("What is your name?");
+		System.out.println("What is your name?"); //extra challenge 
 		String name = scnr.next();
 		
 		do {
@@ -17,7 +17,7 @@ public class LabNum3 {
 			int userNum = scnr.nextInt();
 			
 		if (userNum >= 1 && userNum <= 100) {
-			if (userNum % 2 != 0 && userNum > 60) {
+			if (userNum % 2 != 0 && userNum > 60) { //starting from top down 
 				System.out.println("Output: " + userNum + " Odd and over 60");
 			} else if (userNum % 2 == 0 && userNum > 60) {
 				System.out.println("Output: " + userNum + " Even");
@@ -29,18 +29,20 @@ public class LabNum3 {
 				System.out.println("Odd");
 			}
 		}
+		
+		//this statement makes sure user will put in a number in between 1-100
 		else {
 			System.out.println("Number is out of range - please enter a value between 0 and 100");
 		}
 		
 		System.out.println("Continue? (yes/no)");
-		answer = scnr.next();
+		answer = scnr.next(); //takes the next string 
 		if (answer.equalsIgnoreCase("no")) {
 			System.out.println("Bye!");
 			break;
 		}
 			
-		}while(answer.equalsIgnoreCase("yes"));	
+		}while(answer.equalsIgnoreCase("yes"));	//continues looping
 		
 	
 	}
